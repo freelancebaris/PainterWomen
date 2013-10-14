def8ba9d8b740e4f8aa251e997524219f6ea6be4
ozaydinb <ozaydinbaris@gmail.com>
2013-10-14 14:32:12 +0300

    -sync

---
+++ PainterWomen/Controllers/PublicController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Foundation;
+using Foundation.Abstraction.Services;
+
+namespace PainterWomen.Presentation.Controllers
+{
+    public class PublicController: BaseController
+    {
+        private readonly ISessionHelper _sessionHelper;
+        public PublicController(ISessionHelper sessionHelper)
+        {
+            _sessionHelper = sessionHelper;
+        }
+
+        //public JsonResult GetMenuLinks()
+        //{
+
+        //    if (_sessionHelper.UserContext.SelectedLanguage == Enums.Language.Tr)
+        //    {
+                
+        //    }
+        //}
+    }
+}
