ff549a3eb02cee0321f09b525d4dfa0a8fc405cd
ozaydinb <ozaydinbaris@gmail.com>
2013-10-14 16:03:28 +0300

    -layout changed

---
+++ PainterWomen/Controllers/GalleryController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PainterWomen.Presentation.Controllers
+{
+    public class GalleryController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.MenuIndex = 3;
+            return View();
+        }
+
+    }
+}
--- PainterWomen/Scripts/Services/HomeService.js
+++ PainterWomen/Scripts/Services/HomeService.js
@@ -1,14 +1,15 @@
 ﻿painterapp.factory('homeService', function($http,$q) {
     return {
         getLinks: function (selectedPage) {
+            debugger;
             var deferred = $q.defer();
             $http({
-                url: getBaseUrl() + 'Ajax' + '/Public/GetMenuLinks',
+                url: getBaseUrl() + 'Ajax/Public/GetMenuLinks',
                 method: 'POST'
             }).success(function (response) {
                 debugger;
                 if (response.success) {
-                    response.menuList[selectedPage].className += " current";
+                    response.menuList[selectedPage].ClassName += " current";
                     deferred.resolve(response.menuList);
                 }
             });
