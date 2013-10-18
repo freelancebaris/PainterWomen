88545b837caa8f74a8e0792d332d9505e2ade44c
hanifitayfur <hanifitayfur@gmail.com>
2013-10-18 18:44:52 +0300

    ss

---
+++ Repository/DataContext.cs
@@ -0,0 +1,24 @@
+﻿namespace Repository
+{
+    using Foundation;
+
+    using ServiceStack.OrmLite;
+
+    public abstract class DataContext
+    {
+        protected static string SqliteFileDb = Strings.Connection.ConnectionString;
+        protected static IOrmLiteDialectProvider DefaultProvider = SqlServerDialect.Provider;
+        public static string DefaultConnection = SqliteFileDb;
+
+        protected void CreateTableIfNotExists<TEntity>() where TEntity : new()
+        {
+            OrmLiteConfig.DialectProvider = DefaultProvider;
+            var dbFactory = new OrmLiteConnectionFactory(SqliteFileDb, DefaultProvider);
+            using (var db = dbFactory.Open())
+            {
+                db.CreateTableIfNotExists<TEntity>();
+            }
+        }
+
+    }
+}
